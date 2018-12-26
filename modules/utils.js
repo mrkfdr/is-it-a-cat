760a7424d4523c17f68e18fcab89c2897369939e
MarkFdr <markfeder@gmail.com>
2018-12-26 05:00:08 +0200

    added yaml upgrade to 10 fixes

--- modules/utils.js
+++ modules/utils.js
@@ -1,5 +1,5 @@
 exports.bufferImage = function (b64Img) {
     var data = b64Img.replace(/^data:image\/\w+;base64,/, "");
-    var buf = new Buffer(data, 'base64');
+    var buf = new Buffer.from(data, 'base64');
     return buf
 };
